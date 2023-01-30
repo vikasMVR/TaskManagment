f13830e7f0912fe11f6e895e46a16a4bcb80f66f
Anurag Biswal <anugbswl@gmail.com>
2023-01-30 23:47:50 +0530

    initial commit getting ready with all installations

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className="h-20 w-30 bg-indigo-400">tailwindcss web components</div>
+    </div>
+  )
+}
+
+export default App
